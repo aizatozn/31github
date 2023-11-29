11e1fc55dadd31c977088ee9a785d538f4ebdc2e
Aizat <aizatozn@gmail.com>
2023-11-29 22:17:27 +0600

    location manager 1

---
+++ brightsky/Views/CurrentWeatherView.swift
@@ -0,0 +1,20 @@
+//
+//  CurrentWeatherView.swift
+//  brightsky
+//
+//  Created by Aizat Ozbekova on 29/11/23.
+//
+
+import UIKit
+
+class CurrentWeatherView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
