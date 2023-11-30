dfd4909adb4cc19041402ce2c194efbeb80e9492
Aizat <aizatozn@gmail.com>
2023-11-30 17:17:37 +0600

    Location Manager

--- brightsky/Controllers/WeatherViewController.swift
+++ brightsky/Controllers/WeatherViewController.swift
@@ -13,6 +13,20 @@ class WeatherViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpView()
+        getLocation()
+    }
+    
+    private func getLocation() {
+        LocationManager.shared.getCurrentLocation { location in
+            print(String(describing: location))
+            
+            WeatherManager.shared.getWeather(for: location)
+            
+        }
+    }
+    
+    private func setUpView() {
         view.backgroundColor = .systemBackground
         
         view.addSubview(primaryView)
--- brightsky/Managers/LocationManager.swift
+++ brightsky/Managers/LocationManager.swift
@@ -14,18 +14,33 @@ final class LocationManager: NSObject, CLLocationManagerDelegate {
     
     static let shared = LocationManager()
     
-    public func getCurrentLocation() {
+    private var locationFetchCompletion: ((CLLocation) ->Void)?
+    
+    private var location: CLLocation? {
+        didSet {
+            guard let location else {
+                return
+            }
+            locationFetchCompletion?(location)
+        }
+    }
+    
+    public func getCurrentLocation(completion: @escaping (CLLocation) -> Void) {
+        
+        self.locationFetchCompletion = completion
+        
         manager.requestWhenInUseAuthorization()
         manager.delegate = self
         manager.startUpdatingLocation()
     }
     
-    //MARK: - Location
+    // MARK: - Location
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let location = locations.first else {
             return
         }
+        self.location = location
         manager.stopUpdatingLocation()
     }
 }
