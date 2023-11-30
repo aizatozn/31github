237cc22e2c3ec9618a76aa2c80a73b3ce29eb3d3
Aizat <aizatozn@gmail.com>
2023-11-30 16:32:23 +0600

    location manager2

--- brightsky/Managers/LocationManager.swift
+++ brightsky/Managers/LocationManager.swift
@@ -8,6 +8,24 @@
 import CoreLocation
 import Foundation
 
-final class LocationManager: NSObject {
+final class LocationManager: NSObject, CLLocationManagerDelegate {
+    
+    private let manager = CLLocationManager()
+    
     static let shared = LocationManager()
+    
+    public func getCurrentLocation() {
+        manager.requestWhenInUseAuthorization()
+        manager.delegate = self
+        manager.startUpdatingLocation()
     }
+    
+    //MARK: - Location
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.first else {
+            return
+        }
+        manager.stopUpdatingLocation()
+    }
+}
