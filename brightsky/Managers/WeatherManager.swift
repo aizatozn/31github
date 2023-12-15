6551c7732fa3f4aaf1f9785fd4055cce5126d9a0
Aizat <aizatozn@gmail.com>
2023-12-15 21:34:07 +0600

    CurrentWeatherView

--- brightsky/Managers/WeatherManager.swift
+++ brightsky/Managers/WeatherManager.swift
@@ -39,4 +39,5 @@ final class WeatherManager {
             }
         }
     }
+    
 }
--- brightsky/Views/CurrentWeatherView.swift
+++ brightsky/Views/CurrentWeatherView.swift
@@ -154,4 +154,8 @@ extension CurrentWeatherView: UICollectionViewDataSource {
             return cell
         }
     }
+    
+    
+    
+    
 }
