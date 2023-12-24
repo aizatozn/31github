e30a8016bd4bbac87cc5322c0c431fbc2f88e680
Aizat <aizatozn@gmail.com>
2023-12-24 14:57:07 +0600

    logoButtonTapped

--- brightsky/Controllers/TabViewController.swift
+++ brightsky/Controllers/TabViewController.swift
@@ -72,4 +72,9 @@ final class HomeViewController: UIViewController {
     @objc private func menuButtonTapped() {
         // Обработка нажатия кнопки меню
     }
+    
+    
+    @objc private func logoButtonTapped() {
+        // Обработка нажатия на логотип
+    }
 }
